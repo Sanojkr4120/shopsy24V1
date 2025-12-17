3b176063efb51fbd7f780f7ccb1512392b65ec32
Sanojkr4120 <sanoj2003feb11@gmail.com>
2025-12-17 08:07:42 +0530

    update

--- shopsy24/frontend/src/pages/Menu.jsx
+++ shopsy24/frontend/src/pages/Menu.jsx
@@ -340,7 +340,7 @@ const Menu = () => {
                 <motion.div
                     initial={{ scale: 0, opacity: 0 }}
                     animate={{ scale: 1, opacity: 1 }}
-                    className="fixed bottom-8 right-4 sm:right-8 z-50"
+                    className="fixed top-24 right-4 sm:right-8 z-50"
                 >
                     <button
                         onClick={proceedToCheckout}
